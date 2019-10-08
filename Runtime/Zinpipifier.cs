22123a08daf585181092e228e23cb1e2d03a40fc
Harvey Ball <me@harveyball.com>
2019-10-08 20:59:13 +0100

    first commit

---
+++ Runtime/Zinpipifier.cs
@@ -0,0 +1,13 @@
+﻿namespace TheStonefox.Test.Zinpipa
+{
+    using TheStonefox.Test.Mabimbey;
+
+    public class Zinpipifier
+    {
+        public virtual string Zinpipify()
+        {
+            Mabimbificator m = new Mabimbificator();
+            return "A " + m.Mabimbificate();
+        }
+    }
+}
